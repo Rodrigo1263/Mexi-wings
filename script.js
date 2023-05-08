07fe84d27358ef23130eeab3e77cbbd54c849e86
Rodrigo Muñiz <Rodrigo1263>
2023-05-08 00:31:54 -0600

    Primera iplementacion Java

---
+++ script.js
@@ -0,0 +1,72 @@
+/*Para dar opciones del carrito de compra*/
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const elementos2 = document.getElementById('lista-2');
+const lista=document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventListeners();
+/*Darle sus funciones de accion del carro de compra */
+function cargarEventListeners(){
+    elementos1.addEventListener('click',comprarElemento);
+    elementos2.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+/*Crear funciones necesarias de compra del elemento */
+function comprarElemento(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento){
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+    insestarCarrito(infoElemento);
+}
+
+function insestarCarrito(elemento){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=100 >
+        </td>
+        <td>
+            ${elemento.titulo}
+        </td>
+        <td>
+            ${elemento.precio}
+        </td>
+        <td>
+            <a herf="#" class="borrar" data-id="${elemento.id}">X</a>
+        </td>
+    `;
+
+    lista.appendChild(row);
+}
+/*Funcion para eliminar elemento*/
+
+function eliminarElemento(e){
+    e.preventDefault();
+    let elemento,
+        elementoId;
+    
+    if(e.target.classList.contains('borrar')){
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector('a').getAttribute('data-id');
+    }
+}
+function vaciarCarrito(){
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+return false;
+}
